dd3db39591fbcbf4af5624da7fa39eddac17cf27
allisons-web <146009699+allisons-web@users.noreply.github.com>
2023-10-09 13:02:49 -0400

    Add files via upload
    web ring added

--- s1fp/time.js
+++ s1fp/time.js
@@ -1,11 +1,11 @@
 function printTime() {
   var d = new Date();
-  var hours = d.getHours() % 12;
+  var hours = d.getHours();
   var mins = d.getMinutes();
   if (mins < 10) {
     mins = "0" + mins;
   }
-  var ampm = hours <= 12 ? 'PM' : 'AM';
+  var ampm = hours <= 12 ? 'AM' : 'PM';
   clock = document.getElementById("time")
-  clock.innerHTML = hours+":"+mins+" "+ampm;
+  clock.innerHTML = (hours % 12)+":"+mins+" "+ampm;
 }
