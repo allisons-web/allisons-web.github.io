99a3f19ae23549961f1f6e3ef9c0cfeb658de29e
allisons-web <146009699+allisons-web@users.noreply.github.com>
2023-12-21 21:02:50 -0500

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,10 @@ let caps = [
     "Good day!",
     "Thanks for stopping by today.",
     "It's lovely to see you!",
-    "What are you waiting for?"
+    "My portfolio is divided into three sections,",
+    "each represents a segment of the web.",
+    "What are you waiting for?",
+    "Click around and explore!"
 ]
 
 var d = new Date();
@@ -17,7 +20,7 @@ let i = 0
 
 function capRotate() {
     document.getElementById('caption-line').innerText = caps[i];
-    if (i == 3) {
+    if (i == caps.length - 1) {
         i = 0;
     } else {
         i++;
